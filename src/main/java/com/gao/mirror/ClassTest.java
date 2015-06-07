fe4392802db6d96b3ba0853525f5ede1890ebe65
忘尘 <wangchen.gpx@taobao.com>
2015-06-07 13:54:46 +0800

    添加了测试class的反射和具体的内部实现逻辑

--- src/main/java/com/gao/mirror/ClassTest.java
+++ src/main/java/com/gao/mirror/ClassTest.java
@@ -1,5 +1,7 @@
 package com.gao.mirror;
 
+import net.vidageek.mirror.dsl.Mirror;
+
 /**
  * User: wangchen
  * Date: 15/6/7
@@ -7,7 +9,15 @@ package com.gao.mirror;
  * 用来测试对于class的操作反射工具
  */
 public class ClassTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        Class<?> aClass = new Mirror().reflectClass("java.lang.String");
+
+        System.out.println(aClass.getName());
+
+        //其实底层实现也是基于反射的基本方法类实现的
+        //等价于下面的代码逻辑
+        Class<?> aClass1 = Class.forName("java.lang.String", false, null);
 
     }
 }
