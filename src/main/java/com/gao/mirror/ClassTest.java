60de11cf31bc9330962ad33232684a160e8bd73d
忘尘 <wangchen.gpx@taobao.com>
2015-06-07 13:46:48 +0800

    Initial commit

---
+++ src/main/java/com/gao/mirror/ClassTest.java
@@ -0,0 +1,13 @@
+package com.gao.mirror;
+
+/**
+ * User: wangchen
+ * Date: 15/6/7
+ * Time: 13:45
+ * 用来测试对于class的操作反射工具
+ */
+public class ClassTest {
+    public static void main(String[] args) {
+
+    }
+}
